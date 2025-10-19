53e14a14e7b36b6889975075ca198cba07f896cd
Operator-Syn <johnronanbeira100404@gmail.com>
2025-10-19 13:55:03 +0800

    PTP-66 Separated Modal buttons into dynamic component

---
+++ views/src/data/AppointmentsPageModalButtons.js
@@ -0,0 +1,20 @@
+export const ConfirmButton = [
+    {
+        text: "Confirm",
+        variant: "primary",
+        onClick: (closeModal) => {
+            closeModal(); // optionally close the modal
+        },
+    },
+];
+
+export const CloseButton = [
+    {
+        text: "Close",
+        variant: "secondary",
+        // the onClick receives the close function automatically from the component
+        onClick: (closeModal) => {
+            closeModal();
+        },
+    },
+]
