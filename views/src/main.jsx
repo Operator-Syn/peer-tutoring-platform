b536d51024e661135faad43ecb51b73951950444
Operator-Syn <johnronanbeira100404@gmail.com>
2025-10-19 13:55:49 +0800

    PTP-66 Major Checkpoint

--- views/src/main.jsx
+++ views/src/main.jsx
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
+import AppTest from './components/testFiles/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <AppTest />
   </StrictMode>,
 )
