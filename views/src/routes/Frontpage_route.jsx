0a45c46f630db2ddf122f4ab0197773b4eca7d95
Operator-Syn <johnronanbeira100404@gmail.com>
2025-11-04 10:59:37 +0800

    PTP-66 Transferred all routes to App.jsx

--- views/src/routes/Frontpage_route.jsx
+++ views/src/routes/Frontpage_route.jsx
@@ -1,28 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MasterLayout from "../Layouts/masterlayout";
 import HomePage from "../pages/header/HomePage/HomePage";
 import About from "../pages/header/About";
 import Events from "../pages/header/Events";
 import Messages from "../pages/header/Messages";
 import Report from "../pages/header/Report";
-import TuteeAppointmentsPage from "../components/TuteeAppointmentsPage/TuteeAppointmentsPage";
 
-function Frontroute() {
+function FrontpageRoute() {
   return (
     <Routes>
-      
-      <Route element={<MasterLayout />}>
-        <Route path = "/" element={<HomePage />} />
-        <Route path = "/About" element={<About />} />
-        <Route path = "/Events" element={<Events />} />
-        <Route path = "/Messages" element={<Messages />} />
-        <Route path = "/Report" element={<Report />} />
-        <Route path = "/Appointments" element={<TuteeAppointmentsPage/>}/>
-       
-      </Route>
+      <Route index element={<HomePage />} />
+      <Route path="About" element={<About />} />
+      <Route path="Events" element={<Events />} />
+      <Route path="Messages" element={<Messages />} />
+      <Route path="Report" element={<Report />} />
     </Routes>
   );
 }
 
-export default Frontroute;
+export default FrontpageRoute;
