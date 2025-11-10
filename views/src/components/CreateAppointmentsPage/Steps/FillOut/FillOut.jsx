61028f0e0830163a73fb75222f9ec424b8371d9a
Operator-Syn <johnronanbeira100404@gmail.com>
2025-11-10 15:53:43 +0800

    PTP-69 FillOut progress

--- views/src/components/CreateAppointmentsPage/Steps/FillOut/FillOut.jsx
+++ views/src/components/CreateAppointmentsPage/Steps/FillOut/FillOut.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./FillOut.css";
 
 export default function FillOut({ data, update }) {
     return (
-        <div>
-            <h3>Step 1: Fill Out</h3>
-            <label>
-                First Name:
+        <div className="p-3">
+            <h3 className="mb-3">Step 1: Fill Out</h3>
+
+            <div className="border-label-group mb-3">
+                <label className="form-label border-label">First Name</label>
                 <input
                     type="text"
+                    className="form-control"
                     value={data.firstName}
                     onChange={(e) => update({ firstName: e.target.value })}
                 />
-            </label>
-            <br />
-            <label>
-                Last Name:
+            </div>
+
+            <div className="border-label-group mb-3">
+                <label className="form-label border-label">Last Name</label>
                 <input
                     type="text"
+                    className="form-control"
                     value={data.lastName}
                     onChange={(e) => update({ lastName: e.target.value })}
                 />
-            </label>
+            </div>
         </div>
     );
 }
