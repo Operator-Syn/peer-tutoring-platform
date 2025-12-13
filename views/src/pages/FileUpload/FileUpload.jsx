b058240b0d19a5e3656d6f1c62a3acd9564d1f51
Nel <neilzeith@gmail.com>
2025-12-14 00:04:42 +0800

    PTP-73 file select functionality

--- views/src/pages/FileUpload/FileUpload.jsx
+++ views/src/pages/FileUpload/FileUpload.jsx
@@ -1,6 +1,6 @@
 
 import './FileUpload.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import Select from 'react-select';
 import BasicButton from '../../components/BasicButton/BasicButton';
@@ -9,6 +9,7 @@ export default function FileUpload() {
     const API_URL = import.meta.env.VITE_API_BASE_URL;
 
     const [files, setFiles] = useState([]);
+    const fileInputRef =  useRef(null);
     const [courses, setCourses] = useState([]);
     const [selectedCourse, setSelectedCourse] = useState(null);
 
@@ -19,6 +20,24 @@ export default function FileUpload() {
         setCourses((data.courses || []).map(c => ({ value: c, label: c })));
     }
 
+    const handleChooseFile = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (e) => {
+        const newFiles = Array.from(e.target.files);
+        setFiles(prevFiles => {
+            // Remove any old file with the same name as a new file
+            const filteredPrev = prevFiles.filter(
+                oldFile => !newFiles.some(newFile => newFile.name === oldFile.name)
+            );
+            return [...filteredPrev, ...newFiles];
+        });
+    };
+
+    const deleteFile = (fileName) => {
+        setFiles(prevFiles => prevFiles.filter(file => file.name !== fileName));
+    }
     
 
     return (
@@ -35,15 +54,25 @@ export default function FileUpload() {
                         <InputFormFU label="Related Course">
                             <Select isClearable placeholder="Course" options={courses} onInputChange={e => {retrieveCourses(e);}} onChange={e => setSelectedCourse(e)} />
                         </InputFormFU>
+
                         <InputFormFU label="Upload File" customClass='upload-file-form'>
                             <div className='upload-file-container'>
-                                <BasicButton text="Choose File" style={{padding: "0px", width: "40px", height: "40px", boxShadow: "none"}} light={true}>
+                                <BasicButton text="Choose File" style={{padding: "0px", width: "40px", height: "40px", boxShadow: "none"}} light={true} onClick={handleChooseFile}>
                                     <img src="https://www.svgrepo.com/show/533718/upload.svg" alt="upload icon" style={{width: "40px"}} />
                                 </BasicButton>
-                                <FileList files={files} />
+                                <input
+                                    type="file"
+                                    accept='.png,.jpeg,.jpg'
+                                    multiple
+                                    ref={fileInputRef}
+                                    style={{ display: 'none' }}
+                                    onChange={handleFileChange}
+                                />
+                                <FileList files={files} deleteFileFunc={deleteFile} />
                             </div>
-                            
                         </InputFormFU>
+
+
                     </div>
                     <div className='input-forms-right-fu'>
                         <InputFormFU label="Note Description" customClass='note-description-form'>
@@ -76,34 +105,23 @@ function InputFormFU({ children, label, style, customClass}) {
     );
 }
 
-function FileList({ files }) {
+function FileList({ files, deleteFileFunc }) {
     return (
         <div className='file-list' style={{border: "1px solid #ccc", borderRadius: "5px", width: "100%", flex: "1"}}>
-            {/* {files.map((file, index) => (
-                <div key={index} className='file-item'>
-                    {file.name}
-                </div>
-            ))} */}
-            <FileItem file={{ name: "example_note.pdfsadhasghgashdgasdhasgdhasgd" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
-            <FileItem file={{ name: "example_note.pdf" }} />
+            {files.map((file, index) => (
+                <FileItem key={index} file={file} deleteFileFunc={deleteFileFunc} />
+            ))}
         </div>
     );
 }
 
-function FileItem({ file }) {
+function FileItem({ file, deleteFileFunc }) {
     return (
         <div className='file-item' style={{flex: "1"}}>
             <div style={{overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap", maxWidth: "170px"}}>
             {file.name}
             </div>
-            <BasicButton className="flex" style={{padding: "0", width: "25px", height: "25px", boxShadow: "none", alignItems: "center", justifyContent: "center"}} danger={true} light={true}>
+            <BasicButton className="flex" style={{padding: "0", width: "25px", height: "25px", boxShadow: "none", alignItems: "center", justifyContent: "center"}} danger={true} light={true} onClick={() => deleteFileFunc(file.name)}>
                 <img src="https://www.svgrepo.com/show/310733/delete.svg" alt="delete icon" style={{width: "15px", filter: "invert(23%) sepia(99%) saturate(7492%) hue-rotate(357deg) brightness(97%) contrast(119%)"}} />
             </BasicButton>
         </div>
